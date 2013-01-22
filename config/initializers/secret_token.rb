40e6407a3627e6427c2d1493223cdddf84dfb62c
david raygoza <davidraygoza@iMac-de-david-raygoza.local>
2013-01-22 10:57:00 -0600

    el inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogJumpstart::Application.config.secret_token = '3fd8ea579dd43e432667a66c492cf9828f16207a9196a11232a185ec5c8f8105e7f9f1db1a21fb4a05677e4e465e70a3b82303cfeaff3832d2a028e9a6ab73ed'
